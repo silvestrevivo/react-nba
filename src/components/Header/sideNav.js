f6ee7e187da1359877fb7b339ac08fb901cc0605
silvestrevivo <silvestrevivomillan@gmail.com>
2018-05-15 09:42:33 +0200

    refractoring css and aux component added to sidenav

--- src/components/Header/sideNav.js
+++ src/components/Header/sideNav.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Aux from '../../hoc/aux'
 import SideNav from 'react-simple-sidenav'
 import SideNavItems from './sideNavItems'
 
 const SideNavigation = (props) => {
   return (
-    <div>
+    <Aux>
       <SideNav
         showNav={props.showNav}
         onHideNav={props.onHideNav}
         navStyle={{ backgroundColor: '#242424', maxWidth: '300px' }}>
         <SideNavItems onHideNav={props.onHideNav} />
       </SideNav>
-    </div>
+    </Aux>
   )
 }
 
